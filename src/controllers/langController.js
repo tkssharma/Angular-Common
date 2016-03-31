98f4df3af7827ed4edd5de1b39ce92318850fcb3
tarun <cooltarunsharma111@gmail.com>
2016-03-31 10:29:47 +0530

    lang translator added

--- src/controllers/langController.js
+++ src/controllers/langController.js
@@ -3,17 +3,34 @@
  * can be added to this root module & should be added in every file @ ngTranslate
  * @ LocalController to fetch Translate data
  */
-(function() {
-	angular.module("Angularapps").controller("localController", localController)
-	/* .factory("springRestLocalFetchservice",springRestLocalFetchservice) */
-	.factory("asyncLoader", asyncLoader)
-	.config(config);
+ (function() {
+ 	angular.module("Angularapps").controller("localController", localController)
+ 	/* .factory("springRestLocalFetchservice",springRestLocalFetchservice) */
+ 	.factory("asyncLoader", asyncLoader)
+ 	.config(config);
 
 	//@config
 	config.$inject = [ '$translateProvider' ];
 	function config($translateProvider) {
-		$translateProvider.preferredLanguage('en');
+		//$translateProvider.preferredLanguage('en');
 		$translateProvider.useLoader('asyncLoader');
+
+		var langMap = {
+			'en_AU': 'en',
+			'en_CA': 'en',
+			'en_NZ': 'en',
+			'en_PH': 'en',
+			'en_UK': 'en',
+			'en_US': 'en',
+			'ja_JP': 'ja'
+		};
+
+		$translateProvider
+		.registerAvailableLanguageKeys(['en', 'sp'], langMap)
+		.determinePreferredLanguage();
+		/*console.log($translateProvider.
+		.determinePreferredLanguage());*/
+                                //window.navigator.languages[0]
 
 	}
 
@@ -26,48 +43,48 @@
 
 			var resturl = "lang"+options.key+".json"
 			$http.get(
-					resturl,{cache:false}).success(function(translations) {
-						deferred.resolve(translations);
+				resturl,{cache:false}).success(function(translations) {
+					deferred.resolve(translations);
 						//alert(translations);
 					}).error(function(translations) {
 						deferred.reject(translations);
 					});
 
-			$timeout(function() {
-				deferred.resolve(translations);
-			}, 2000);
+					$timeout(function() {
+						deferred.resolve(translations);
+					}, 2000);
 
-			return deferred.promise;
-		};
+					return deferred.promise;
+				};
 
-	}
+			}
 
 
 	//@controller
 	localController.$inject = [ '$translate', '$scope','$rootScope','$state'];
 	function localController($translate, $scope,$rootScope,$state) {
-		$scope.defaultlanguage = true;	
+		$scope.defaultlanguage = true;
 		$scope.state= $state;
 		$scope.changeLanguage = function(langKey) {
 
-			
-				if(langKey === 'en')
-				{
-					$rootScope.lang= "en";
-					$scope.defaultlanguage = true;	
+
+			if(langKey === 'en')
+			{
+				$rootScope.lang= "en";
+				$scope.defaultlanguage = true;
 					//changeLanguageFeedbackPanel(langKey);
 				}
 				if(langKey === 'sp')
 				{
 					$rootScope.lang= "sp";
-					$scope.defaultlanguage = false;	
+					$scope.defaultlanguage = false;
 					//changeLanguageFeedbackPanel(langKey);
 				}
-			
 
-			$translate.use(langKey);
-		};
 
-	}
+				$translate.use(langKey);
+			};
 
-})();
+		}
+
+	})();
